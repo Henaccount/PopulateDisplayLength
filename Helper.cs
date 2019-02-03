e7c888e9a7400c20a0258437d7ba7cda67cac186
Henaccount <36333524+Henaccount@users.noreply.github.com>
2019-02-03 23:38:54 +0100

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,114 @@
+﻿//
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2015 Autodesk, Inc.  All rights reserved.
+//
+//  Use of this software is subject to the terms of the Autodesk license 
+//  agreement provided at the time of installation or download, or which 
+//  otherwise accompanies this software in either electronic or hard copy form.   
+//
+//////////////////////////////////////////////////////////////////////////////
+// if just one type of hose exists, shortdescription should be "HOSE"
+
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+
+
+using Autodesk.ProcessPower.DataLinks;
+using Autodesk.ProcessPower.ProjectManager;
+using Autodesk.ProcessPower.PlantInstance;
+using Autodesk.AutoCAD.EditorInput;
+
+using System;
+using PlantApp = Autodesk.ProcessPower.PlantInstance.PlantApplication;
+
+using System.Collections.Generic;
+using Autodesk.ProcessPower.PnP3dObjects;
+using Autodesk.ProcessPower.PnP3dDataLinks;
+
+namespace pssCommands
+{
+    /// <summary>
+    /// Helper class including some static helper functions.
+    /// </summary>
+    /// 
+
+
+    public class Helper
+    {
+        public static Project currentProject { get; set; }
+        public static Document ActiveDocument { get; set; }
+        public static DataLinksManager ActiveDataLinksManager { get; set; }
+        public static DataLinksManager3d dlm3d { get; set; }
+        public static Database db { get; set; }
+        public static Editor ed { get; set; }
+        public static PipingObjectAdder pipeObjAdder { get; set; }
+
+
+        public static bool Initialize()
+        {
+            if (PlantApplication.CurrentProject == null)
+                return false;
+
+
+            currentProject = PlantApp.CurrentProject.ProjectParts["Piping"];
+            ActiveDataLinksManager = currentProject.DataLinksManager;
+            dlm3d = DataLinksManager3d.Get3dManager(ActiveDataLinksManager);
+            pipeObjAdder = new PipingObjectAdder(dlm3d, db);
+            ActiveDocument = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            db = ActiveDocument.Database;
+            ed = ActiveDocument.Editor;
+            return true;
+        }
+
+        public static void Terminate()
+        {
+            currentProject = null;
+            ActiveDataLinksManager = null;
+            ActiveDocument = null;
+            db = null;
+            ed = null;
+        }
+
+        public static IDictionary<string, double> ReadInDict(string thestring, bool fillvalues)
+        {
+            string whole_file = thestring;
+
+            IDictionary<string, double> dict = new Dictionary<string, double>();
+
+            // Split into lines.
+            string[] lines = whole_file.Split(new char[] { ',' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // See how many rows and columns there are.
+            int num_rows = lines.Length;
+
+            // Load the dictionary.
+            for (int r = 0; r < num_rows; r++)
+            {
+                string[] line_r = lines[r].Split(new char[] { '=' });
+
+                if (fillvalues)
+                    dict[line_r[0]] = Convert.ToDouble(line_r[1]);
+                else
+                    dict[line_r[0]] = 0.0;
+            }
+
+            // Return the values.
+            return dict;
+        }
+
+    }
+
+    // Helper class to workaround a Hashtable issue: 
+    // Can't change values in a foreach loop or enumerator
+    class CBoolClass
+    {
+        public CBoolClass(bool val) { this.val = val; }
+        public bool val;
+        public override string ToString() { return (val.ToString()); }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿#region Namespaces
+
+using System;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+using System.Collections.Specialized;
+using Autodesk.ProcessPower.ProjectManager;
+using Autodesk.ProcessPower.PlantInstance;
+using Autodesk.AutoCAD.Runtime;
+
+#endregion
+
+//V0
+//v1:added DisplayElevation
+
+namespace pssCommands
+{
+    public class Program
+    {
+        public static int precision = 4;
+
+
+        public static void populateDisplayLength()
+        {
+            
+            Helper.Initialize();
+
+            try
+            {
+                if (!PnPProjectUtils.GetActiveDocumentType().Equals("Piping"))
+                {
+                    Helper.ed.WriteMessage("\n This tool works only on Plant 3D Piping drawings!");
+                    return;
+                }
+
+                if (Autodesk.ProcessPower.AcPp3dObjectsUtils.ProjectUnits.CurrentLinearUnit == 1) //1=mm,2=in
+                    precision = 0;
+                else
+                    precision = 4;
+
+                Helper.ed.WriteMessage("\n AUTODESK PROVIDES THIS PROGRAM \"AS IS\" AND WITH ALL FAULTS.");
+                Helper.ed.WriteMessage("\n AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF");
+                Helper.ed.WriteMessage("\n MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.");
+                Helper.ed.WriteMessage("\n DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE");
+                Helper.ed.WriteMessage("\n UNINTERRUPTED OR ERROR FREE.");
+
+                
+                using (Transaction tr = Helper.db.TransactionManager.StartTransaction())
+                {
+
+                    TypedValue[] filterlist = new TypedValue[1];
+
+                    filterlist[0] = new TypedValue(0, "ACPPPIPE");
+
+                    SelectionFilter filter = new SelectionFilter(filterlist);
+
+                    PromptSelectionResult selRes = Helper.ed.SelectAll(filter);
+
+                    if (selRes.Status != PromptStatus.OK)
+                    { return; }
+
+                    ObjectId[] objIdArray = selRes.Value.GetObjectIds();
+
+                    foreach (ObjectId id in objIdArray)
+                    {
+                        try
+                        {
+                            StringCollection theKeys = new StringCollection();
+                            StringCollection theValues = new StringCollection();
+                            Entity ent = tr.GetObject(id, OpenMode.ForWrite) as Entity;
+                            PlantProject currentProj = PlantApplication.CurrentProject;
+
+                            String length = "";
+                            String positionz = "";
+                            theKeys.Add("Length");
+                            theKeys.Add("Position Z");
+                            theValues = Helper.ActiveDataLinksManager.GetProperties(id, theKeys, true);
+                            length = theValues[0];
+                            positionz = theValues[1];
+
+                            String displaylength = "";
+                            String displayelevation = "";
+
+                            displaylength = Converter.DistanceToString(Convert.ToDouble(length), DistanceUnitFormat.Current, precision);
+                            if (Autodesk.ProcessPower.AcPp3dObjectsUtils.ProjectUnits.CurrentLinearUnit == 1)
+                                displayelevation = Math.Round(Convert.ToDouble(positionz), precision).ToString();
+                            else
+                                displayelevation = Math.Round(Convert.ToDouble(positionz) / 12, 2).ToString() + "'";
+
+                            try
+                            {
+                                theKeys = new StringCollection();
+                                theValues = new StringCollection();
+                                theKeys.Add("DisplayLength");
+                                theValues.Add(displaylength);
+                                Helper.ActiveDataLinksManager.SetProperties(id, theKeys, theValues);
+                            }
+                            catch (System.Exception) { }
+
+                            try
+                            {
+                                theKeys = new StringCollection();
+                                theValues = new StringCollection();
+                                theKeys.Add("DisplayElevation");
+                                theValues.Add(displayelevation);
+                                Helper.ActiveDataLinksManager.SetProperties(id, theKeys, theValues);
+                            }
+                            catch (System.Exception) { }
+
+
+                        }
+                        catch (System.Exception e)
+                        {
+                            System.Diagnostics.StackTrace trace = new System.Diagnostics.StackTrace(e, true);
+                            Helper.ed.WriteMessage(trace.ToString());
+                            Helper.ed.WriteMessage("\nLine: " + trace.GetFrame(0).GetFileLineNumber());
+                            Helper.ed.WriteMessage("\npipe error: " + e.Message);
+                        }
+
+
+                    }
+
+                    tr.Commit();
+                }
+
+                Helper.ed.WriteMessage("\nScript finished");
+            }
+            catch (System.Exception e)
+            {
+                System.Diagnostics.StackTrace trace = new System.Diagnostics.StackTrace(e, true);
+                Helper.ed.WriteMessage(trace.ToString());
+                Helper.ed.WriteMessage("\nLine: " + trace.GetFrame(0).GetFileLineNumber());
+                Helper.ed.WriteMessage("\nscript error: " + e.Message);
+            }
+            finally
+            {
+                Helper.Terminate();
+            }
+        }
+
+
+    }
+
+
+
+
+}
+
